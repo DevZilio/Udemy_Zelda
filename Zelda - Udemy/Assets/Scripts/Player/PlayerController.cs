8c83e41d2f987409193b65c6148698e56c98b35c
DevZilio <105543270+DevZilio@users.noreply.github.com>
2023-10-27 14:09:25 +0100

    Player animation run and walk

--- Zelda - Udemy/Assets/Scripts/Player/PlayerController.cs
+++ Zelda - Udemy/Assets/Scripts/Player/PlayerController.cs
@@ -11,9 +11,14 @@ public class PlayerController : MonoBehaviour
     public float movimentSpeed = 3f;
     private Vector3 _direction;
 
+    [Header("Run")]
+    public KeyCode keyRun = KeyCode.LeftShift;
+    public float speedRun = 1.5f;
+
     [Header("Animation")]
     private Animator _animator;
     private bool isWalking;
+    private bool isRunning;
 
    
 
@@ -32,38 +37,48 @@ public class PlayerController : MonoBehaviour
         Attack();
     }
 
-    private void Moviment()
+   private void Moviment()
+{
+    float horizontal = Input.GetAxis("Horizontal");
+    float vertical = Input.GetAxis("Vertical");
+
+    _direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+    if (_direction.magnitude > 0.1f)
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-
-        _direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        if(_direction.magnitude > 0.1f)
-        {
-            //Identifica o angulo e converte para radiano
-            float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
-            //Utiliza o angulo encontrado e faz o objeto virar
-            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
-            isWalking = true;
-        }
-        else //(_direction.magnitude <= 0.1f)
-        {
-            isWalking = false;
-        }
-
-
-        _controller.Move(_direction * movimentSpeed * Time.deltaTime);
-        _animator.SetBool("isWalking", isWalking);
+        // Identifica o ângulo e converte para radiano
+        float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+        // Utiliza o ângulo encontrado e faz o objeto virar
+        transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+        isWalking = true;
     }
+    else
+    {
+        isWalking = false;
+    }
+
+    if (Input.GetKey(keyRun) && isWalking)
+    {
+        isRunning = true;
+        isWalking = false;
+    }
+    else
+    {
+        isRunning = false;
+    }
+
+    _controller.Move(_direction * (isRunning ? speedRun : movimentSpeed) * Time.deltaTime);
+    _animator.SetBool("isWalking", isWalking);
+    _animator.SetBool("isRunning", isRunning);
+}
+
 
     private void Attack()
     {
          if(Input.GetKeyDown(KeyCode.Space))
-    {
-        _animator.SetTrigger("Attack");
-    }
+         {
+             _animator.SetTrigger("Attack");
+         }
     }
 
 
